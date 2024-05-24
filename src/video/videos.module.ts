e153c77a526bf96fb033e63f9387bf5eb9545122
nundung <jjy809123@naver.com>
2024-05-24 21:55:17 +0900

    Feat: videosModule 생성

---
+++ src/video/videos.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VideoRepository } from './video.repository';
+
+@Module({
+    imports: [],
+    controllers: [],
+    providers: [],
+})
+export class VideosModule {}
