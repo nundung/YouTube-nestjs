d4f2ee911a5824d39a2b28a1ebe5be9283152ec3
nundung <jjy809123@naver.com>
2024-05-24 17:34:57 +0900

    Feat: createVideo 매서드 생성

---
+++ src/video/videos.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { VideoRepository } from './video.repository';
+import { CreateVideoDto } from './dto/create-video.dto';
+import { User } from 'src/auth/user.entity';
+import { Video } from './video.entity';
+
+@Injectable()
+export class VideosService {
+    constructor(private videoRepository: VideoRepository) {}
+
+    async createVideo(
+        createVideoDto: CreateVideoDto,
+        user: User,
+    ): Promise<Video> {
+        return this.videoRepository.CreateVideo(createVideoDto, user);
+    }
+}
