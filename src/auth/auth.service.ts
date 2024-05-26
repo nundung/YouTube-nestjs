a154212fcde7536db961d3c1a04e14c2129ea8cd
nundung <jjy809123@naver.com>
2024-05-26 22:14:35 +0900

    Feat: auth 기능 생성

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { AuthCredentialsDto } from './auth-credential.dto';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository,
+        private jwtService: JwtService,
+    ) {}
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.createUser(authCredentialsDto);
+    }
+
+    async signIn(
+        authCredentialsDto: AuthCredentialsDto,
+    ): Promise<{ accessToken: string }> {
+        const { name, pw } = authCredentialsDto;
+        const user = await this.userRepository.findOne({
+            where: { name: name },
+        });
+
+        if (!user || !(await bcrypt.compare(pw, user.password))) {
+            throw new UnauthorizedException('login failed');
+        }
+        const payload = { name };
+        const accessToken = await this.jwtService.sign(payload);
+
+        return { accessToken };
+    }
+}
