c7d8cb1854b076073809975d713af65cf5415e63
nundung <jjy809123@naver.com>
2024-05-26 17:08:24 +0900

    Feat: videoModule에 import 추가

--- src/video/videos.module.ts
+++ src/video/videos.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VideoRepository } from './video.repository';
+import { VideoController } from './videos.controller';
+import { VideosService } from './videos.service';
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [],
+    imports: [TypeOrmModule.forFeature([VideoRepository])],
+    controllers: [VideoController],
+    providers: [VideosService],
 })
 export class VideosModule {}
