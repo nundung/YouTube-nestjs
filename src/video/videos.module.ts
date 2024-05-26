dbad7f6c6c727320d7dd82850300861b95497fb8
nundung <jjy809123@naver.com>
2024-05-26 18:48:11 +0900

    Fix: module import문 수정

--- src/video/videos.module.ts
+++ src/video/videos.module.ts
@@ -3,10 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VideoRepository } from './video.repository';
 import { VideoController } from './videos.controller';
 import { VideosService } from './videos.service';
+import { Video } from './video.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([VideoRepository])],
+    imports: [
+        TypeOrmModule.forFeature([Video]),
+        TypeOrmModule.forFeature([VideoRepository]),
+    ],
     controllers: [VideoController],
-    providers: [VideosService],
+    exports: [VideosService],
+    providers: [VideosService, VideoRepository],
 })
 export class VideosModule {}
