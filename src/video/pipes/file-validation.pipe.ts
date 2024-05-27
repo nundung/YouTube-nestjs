b030e225b0c0f8f0d70f829981fed7265cf70676
nundung <jjy809123@naver.com>
2024-05-27 22:41:21 +0900

    Feat: fileValidation생성

---
+++ src/video/pipes/file-validation.pipe.ts
@@ -0,0 +1,28 @@
+import {
+    PipeTransform,
+    Injectable,
+    ArgumentMetadata,
+    BadRequestException,
+} from '@nestjs/common';
+
+@Injectable()
+export class FileValidationPipe implements PipeTransform {
+    transform(value: any, metadata: ArgumentMetadata) {
+        const fiftyMb = 50 * 1024 * 1024;
+        if (value.sixe > fiftyMb) {
+            throw new BadRequestException(
+                'File size too large. Max size is 50MB.',
+            );
+        }
+
+        // 파일이 동영상인지 확인 (확장자가 비디오인 경우로 가정)
+        const videoExtensions = ['.mp4', '.avi', '.mov', '.wmv', '.mkv'];
+        const fileExtension = value.originalname.split('.').pop().toLowerCase();
+        if (!videoExtensions.includes(fileExtension)) {
+            throw new BadRequestException(
+                'Invalid file type. Only video files are allowed.',
+            );
+            return true;
+        }
+    }
+}
