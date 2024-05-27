c9376abcd98c076dfd9fff4286db6c3990bd8fb1
nundung <jjy809123@naver.com>
2024-05-27 22:39:36 +0900

    Feat: video관련 매서드 controller 생성

--- src/video/videos.controller.ts
+++ src/video/videos.controller.ts
@@ -1,10 +1,26 @@
-import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Logger,
+    Param,
+    Post,
+    UploadedFile,
+    UseGuards,
+    UseInterceptors,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { User } from 'src/auth/user.entity';
 import { Video } from './video.entity';
 import { VideosService } from './videos.service';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import * as path from 'path';
+import { FileValidationPipe } from './pipes/file-validation.pipe';
 
 @Controller('video')
 @UseGuards(AuthGuard())
@@ -12,14 +28,42 @@ export class VideoController {
     private logger = new Logger('Videos');
     constructor(private videosService: VideosService) {}
 
-    @Post()
-    createVideo(
+    @Post('/upload')
+    @UsePipes(ValidationPipe)
+    @UseInterceptors(
+        FileInterceptor('file', {
+            storage: diskStorage({
+                destination: './uploads',
+                filename: (req, file, cb) => {
+                    const ext = path.extname(file.originalname);
+                    const filename = `${Date.now()}${ext}`;
+                    cb(null, filename);
+                },
+            }),
+        }),
+    )
+    async createVideo(
+        @UploadedFile(new FileValidationPipe()) file: Express.Multer.File,
         @Body() createVideoDto: CreateVideoDto,
         @GetUser() user: User,
     ): Promise<Video> {
-        // this.logger.verbose(
-        //     `User ${user.username} creating a new board.Payload: ${JSON.stringify(createBoardDto)}`,
-        // );
+        const file_path = `/uploads/${file.filename}`;
+        createVideoDto.file_path = file_path;
+
+        this.logger.verbose(
+            `User ${user.name} creating a new video. Payload: ${JSON.stringify(createVideoDto)}`,
+        );
+
         return this.videosService.createVideo(createVideoDto, user);
+    }
+
+    @Get()
+    getAllVideos(): Promise<Video[]> {
+        return this.videosService.getAllVideos();
+    }
+
+    @Get('/:id')
+    getVideoById(@Param('id') id: string): Promise<Video> {
+        return this.videosService.getVideoById(id);
     }
 }
