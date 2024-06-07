6e72c4983e734daed8033d4f88abc9d2549fa556
nundung <jjy809123@naver.com>
2024-06-07 17:50:56 +0900

    Fix: appModule 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { VideoController } from './video/videos.controller';
 import { VideosService } from './video/videos.service';
 import { VideosModule } from './video/videos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModuleOptions, PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import * as config from 'config';
@@ -9,9 +10,7 @@ import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { UserRepository } from './auth/user.repository';
-import { VideoRepository } from './video/video.repository';
-import { subscribe } from 'diagnostics_channel';
-import { SubscribeModule } from './subscribe/subscribe.module';
+import { ChannelModule } from './channel/channel.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 const jwtConfig = config.get('jwt');
@@ -24,11 +23,11 @@ const jwtConfig = config.get('jwt');
             signOptions: { expiresIn: jwtConfig.expiresIn },
         }),
         AuthModule,
-        SubscribeModule,
-        VideosModule,
         PrismaModule,
+        ChannelModule,
+        VideosModule,
     ],
     controllers: [VideoController, AuthController],
-    providers: [AuthService, UserRepository, VideoRepository],
+    providers: [AuthService, UserRepository],
 })
 export class AppModule {}
