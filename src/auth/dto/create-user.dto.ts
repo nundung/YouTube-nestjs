c31140298676f83a242b5a4a57d772e8e83158f9
nundung <jjy809123@naver.com>
2024-06-07 21:57:13 +0900

    Feat: CreateUserDto에 swagger example생성

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,12 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Length, Matches } from 'class-validator';
 
 export class CreateUserDto {
     @IsString()
     @Length(4, 20)
+    @ApiProperty({
+        example: 'testId',
+        description: '4-20자',
+    })
     name: string;
 
     @IsString()
     @Length(4, 20)
+    @ApiProperty({
+        example: 'testPw',
+        description: '영어와 숫자로 4-20자',
+    })
     //영어랑 숫자만 가능한 유효성 체크
     @Matches(/^[a-zA-Z0-9]*$/, {
         message: 'password only accepts english and number',
