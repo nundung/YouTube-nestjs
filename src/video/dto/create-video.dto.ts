1a87a8f2e26aa35a07767aebab09b441ed8da139
nundung <jjy809123@naver.com>
2024-06-07 17:45:17 +0900

    Fix: CreateVideoDto 수정

--- src/video/dto/create-video.dto.ts
+++ src/video/dto/create-video.dto.ts
@@ -2,9 +2,6 @@ import { IsNotEmpty } from 'class-validator';
 
 export class CreateVideoDto {
     @IsNotEmpty()
-    file_path: string;
-
-    @IsNotEmpty()
     title: string;
 
     description: string;
