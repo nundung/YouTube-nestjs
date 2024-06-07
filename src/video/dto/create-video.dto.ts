63d966e2253b5adae02ac700259e80198271db51
nundung <jjy809123@naver.com>
2024-06-07 21:56:50 +0900

    Feat: CreateVideoDto에 swagger example생성

--- src/video/dto/create-video.dto.ts
+++ src/video/dto/create-video.dto.ts
@@ -1,8 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateVideoDto {
     @IsNotEmpty()
+    @ApiProperty({
+        example: 'testTitle',
+    })
     title: string;
 
+    @ApiProperty({
+        example: 'testDescription',
+    })
     description: string;
 }
