b27b4dcc2f50cbf54e00106b28cbdada05bbc8e6
nundung <jjy809123@naver.com>
2024-06-07 17:47:24 +0900

    Fix: fileValidationPipe 수정

--- src/video/pipes/file-validation.pipe.ts
+++ src/video/pipes/file-validation.pipe.ts
@@ -7,22 +7,27 @@ import {
 
 @Injectable()
 export class FileValidationPipe implements PipeTransform {
-    transform(value: any, metadata: ArgumentMetadata) {
+    transform(file: Express.Multer.File, metadata: ArgumentMetadata) {
+        if (!file) {
+            throw new BadRequestException('File is required');
+        }
+
         const fiftyMb = 50 * 1024 * 1024;
-        if (value.size > fiftyMb) {
+        if (file.size > fiftyMb) {
             throw new BadRequestException(
                 'File size too large. Max size is 50MB.',
             );
         }
 
         // 파일이 동영상인지 확인 (확장자가 비디오인 경우로 가정)
-        const videoExtensions = ['.mp4', '.avi', '.mov', '.wmv', '.mkv'];
-        const fileExtension = value.originalname.split('.').pop().toLowerCase();
+        const videoExtensions = ['mp4', 'avi', 'mov', 'wmv', 'mkv'];
+        const fileExtension = file.originalname.split('.').pop().toLowerCase();
         if (!videoExtensions.includes(fileExtension)) {
             throw new BadRequestException(
                 'Invalid file type. Only video files are allowed.',
             );
         }
-        return value;
+        console.log(file);
+        return file;
     }
 }
